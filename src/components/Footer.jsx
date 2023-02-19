9ad7cdd8eb51eac5379b2e38d0243b4517898996
Logasakthi <120245015+Logasakthi8@users.noreply.github.com>
2023-02-18 22:56:06 -0800

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const react = require("react");
+
+function Footer() {
+  const CurrYear = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright @ {CurrYear}</p>
+    </footer>
+  );
+}
+export default Footer;
